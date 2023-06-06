f2c51fc4de81697107a1bf42e315662ee1cca989
Alexander Ultsch <83983652+TheBlackDragon4@users.noreply.github.com>
2023-06-06 22:29:58 +0200

    Add quellcode for the counting of the flown rings

---
+++ Assets/Aircraft Physics/Example/Scripts/RingCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingCounter : MonoBehaviour
+{
+    // Für Zugriff auf auf Ringcounter
+    private AirplaneController airplaneController;
+    private int ringCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Für das Aufsummieren des Ring Counters
+        ringCounter = 0;
+        airplaneController = GetComponent<AirplaneController>();
+        airplaneController.SetRingCounter(ringCounter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            ringCounter++;
+            airplaneController.SetRingCounter(ringCounter);
+            Debug.Log("Ringcounter Anzahl: " + ringCounter);
+        }
+    }
+}
